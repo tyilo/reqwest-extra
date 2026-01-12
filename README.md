4e3bf2c685257dc2649f8c8daeb043b8039183e0
Asger Hautop Drewsen <asger@tyilo.com>
2026-01-12 16:07:31 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# reqwest-extra [![Latest Version]][crates.io] [![API Docs]][docs.rs]
+
+Extra utilities for [`reqwest`](https://crates.io/crates/reqwest).
+
+More information about this crate can be found in the [crate documentation][docs.rs].
+
+[Latest Version]: https://img.shields.io/crates/v/reqwest-extra.svg
+[crates.io]: https://crates.io/crates/reqwest-extra
+[API Docs]: https://img.shields.io/docsrs/reqwest-extra.svg
+[docs.rs]: https://docs.rs/reqwest-extra
---
+++ src/lib.rs
@@ -0,0 +1,162 @@
+#![warn(clippy::pedantic)]
+#![warn(missing_docs)]
+//! Extra utilities for [`reqwest`](https://crates.io/crates/reqwest).
+
+use std::{error::Error, fmt::Display};
+
+use bytes::Bytes;
+use reqwest::Response;
+
+/// A [`reqwest::Error`] that may also contain the response body.
+///
+/// Created from a response using the
+/// [`ResponseExt::error_for_status_with_body`] method. Can also be created from
+/// a [`reqwest::Error`].
+///
+/// # Example
+///
+/// ```
+/// use reqwest_extra::{ErrorWithBody, ResponseExt};
+///
+/// async fn fetch_string(url: &str) -> Result<String, ErrorWithBody> {
+///     let response = reqwest::get(url)
+///         .await?
+///         .error_for_status_with_body()
+///         .await?
+///         .text()
+///         .await?;
+///     Ok(response)
+/// }
+///
+/// # #[tokio::main]
+/// # async fn main() {
+///     let err = fetch_string("https://api.github.com/user").await.unwrap_err();
+///     println!("{err}");
+/// # }
+/// ```
+///
+/// Output (line-wrapped for readability):
+/// ```text
+/// HTTP status client error (403 Forbidden) for url (https://api.github.com/user),
+/// body: b"\r\nRequest forbidden by administrative rules.
+/// Please make sure your request has a User-Agent header
+/// (https://docs.github.com/en/rest/overview/resources-in-the-rest-api#user-agent-required).
+/// Check https://developer.github.com for other possible causes.\r\n"
+/// ```
+#[derive(Debug)]
+pub struct ErrorWithBody {
+    inner: reqwest::Error,
+    body: Option<Result<Bytes, reqwest::Error>>,
+}
+
+impl ErrorWithBody {
+    /// Get a reference to the inner [`reqwest::Error`].
+    pub fn inner(&self) -> &reqwest::Error {
+        &self.inner
+    }
+
+    /// Consume the `ErrorWithBody`, returning the inner [`reqwest::Error`].
+    pub fn into_inner(self) -> reqwest::Error {
+        self.inner
+    }
+
+    /// Get a reference to the response body, if available.
+    pub fn body(&self) -> Option<&Result<Bytes, reqwest::Error>> {
+        self.body.as_ref()
+    }
+
+    /// Consume the `ErrorWithBody`, returning the response body, if available.
+    pub fn into_body(self) -> Option<Result<Bytes, reqwest::Error>> {
+        self.body
+    }
+
+    /// Consume the `ErrorWithBody`, returning both the inner [`reqwest::Error`]
+    /// and the response body, if available.
+    pub fn into_parts(self) -> (reqwest::Error, Option<Result<Bytes, reqwest::Error>>) {
+        (self.inner, self.body)
+    }
+}
+
+impl Display for ErrorWithBody {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.inner)?;
+        if let Some(body) = &self.body {
+            match body {
+                Ok(body) => {
+                    write!(f, ", body: {body:?}")?;
+                }
+                Err(body_error) => {
+                    write!(f, ", error reading body: {body_error}")?;
+                }
+            }
+        }
+        Ok(())
+    }
+}
+
+impl Error for ErrorWithBody {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        Some(&self.inner)
+    }
+}
+
+impl From<reqwest::Error> for ErrorWithBody {
+    fn from(err: reqwest::Error) -> Self {
+        ErrorWithBody {
+            inner: err,
+            body: None,
+        }
+    }
+}
+
+/// Extension trait for [`reqwest::Response`] to provide additional
+/// functionality.
+pub trait ResponseExt: sealed::Sealed {
+    /// Like [`reqwest::Response::error_for_status`], but if the response is an
+    /// error, also reads and includes the response body in the returned
+    /// error.
+    ///
+    /// # Example
+    /// ```
+    /// # #[tokio::main]
+    /// # async fn main() {
+    /// use reqwest_extra::ResponseExt;
+    ///
+    /// let response = reqwest::get("https://api.github.com/user").await.unwrap();
+    /// let err = response.error_for_status_with_body().await.unwrap_err();
+    /// println!("{err}");
+    /// # }
+    /// ```
+    ///
+    /// Output (line-wrapped for readability):
+    /// ```text
+    /// HTTP status client error (403 Forbidden) for url (https://api.github.com/user),
+    /// body: b"\r\nRequest forbidden by administrative rules.
+    /// Please make sure your request has a User-Agent header
+    /// (https://docs.github.com/en/rest/overview/resources-in-the-rest-api#user-agent-required).
+    /// Check https://developer.github.com for other possible causes.\r\n"
+    /// ```
+    fn error_for_status_with_body(
+        self,
+    ) -> impl Future<Output = Result<Response, ErrorWithBody>> + Send + Sync + 'static;
+}
+
+impl ResponseExt for Response {
+    async fn error_for_status_with_body(self) -> Result<Response, ErrorWithBody> {
+        match self.error_for_status_ref() {
+            Ok(_) => Ok(self),
+            Err(e) => {
+                let body = self.bytes().await;
+                Err(ErrorWithBody {
+                    inner: e,
+                    body: Some(body),
+                })
+            }
+        }
+    }
+}
+
+mod sealed {
+    pub trait Sealed {}
+    impl Sealed for reqwest::Response {}
+}
